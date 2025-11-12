e17af7ed3fc9a03c688f0bab2d41d5738ddcabd9
soroush83faraz <152191462+soroush83faraz@users.noreply.github.com>
2025-11-12 09:39:23 +0330

    app

---
+++ android/app/src/main/java/com/soroush83faraz/calenderappdemo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.soroush83faraz.calenderappdemo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.soroush83faraz.calenderappdemo',
+  appName: 'Calender App Demo',
+  webDir: 'dist'
+};
+
+export default config;
